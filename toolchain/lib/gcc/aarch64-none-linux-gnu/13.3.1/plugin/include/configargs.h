000dc34d98fdcf36edf3830e4417b8bb0d95a40f
Voicu <77429938+vladvoicux64@users.noreply.github.com>
2024-08-05 17:53:13 +0300

    Downloaded the necessary toolchain.

---
+++ toolchain/lib/gcc/aarch64-none-linux-gnu/13.3.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/data/jenkins/workspace/GNU-toolchain/arm-13-3/src/gcc/configure --target=aarch64-none-linux-gnu --prefix= --with-sysroot=/aarch64-none-linux-gnu/libc --with-build-sysroot=/data/jenkins/workspace/GNU-toolchain/arm-13-3/build-mingw-aarch64-none-linux-gnu/install//aarch64-none-linux-gnu/libc --with-bugurl=https://bugs.linaro.org/ --enable-shared --disable-libatomic --without-cloog --without-isl --disable-libssp --disable-libgomp --disable-libmudflap --disable-libquadmath --enable-checking=yes --enable-languages=c --with-gmp=/data/jenkins/workspace/GNU-toolchain/arm-13-3/build-mingw-aarch64-none-linux-gnu/host-tools --with-mpfr=/data/jenkins/workspace/GNU-toolchain/arm-13-3/build-mingw-aarch64-none-linux-gnu/host-tools --with-mpc=/data/jenkins/workspace/GNU-toolchain/arm-13-3/build-mingw-aarch64-none-linux-gnu/host-tools --enable-fix-cortex-a53-843419 --with-libiconv-prefix=/data/jenkins/workspace/GNU-toolchain/arm-13-3/build-mingw-aarch64-none-linux-gnu/host-tools --enable-mingw-wildcard --with-pkgversion='Arm GNU Toolchain 13.3.Rel1 (Build arm-13.24)'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
